1299e91e04636c006433a08bb40cbbceecfef45d
John Bretherton <270022735@bun2-206-10.local>
2019-08-21 12:34:48 +1200

    Endpoints created

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 const allProducts = require('./data/products');
 
+
+
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
     next();
@@ -21,7 +23,38 @@ app.get('/allProducts', function(req, res){
 //     res.send(allProducts);
 // });
 
+app.get('/product/:id', function(req, res){
+    const productIDparam = req.params.id;
+    let filteredData = [];
+    for (var i = 0; i < allProducts.length; i++) {
+        if(allProducts[i].id.toString() === productIDparam){
+            filteredData.push(allProducts[i]);
+        }
+    }
+    res.send(filteredData);
+});
 
+app.get('/product/edit/:id', function(req, res){
+    const productIDparam = req.params.id;
+    let filteredData = [];
+    for (var i = 0; i < allProducts.length; i++) {
+        if(allProducts[i].id.toString() === productIDparam){
+            filteredData.push(allProducts[i]);
+        }
+    }
+    res.send(filteredData);
+});
+
+app.get('/product/delete/:id', function(req, res){
+    const productIDparam = req.params.id;
+    let filteredData = [];
+    for (var i = 0; i < allProducts.length; i++) {
+        if(allProducts[i].id.toString() === productIDparam){
+            filteredData.push(allProducts[i]);
+        }
+    }
+    res.send(filteredData);
+});
 
 app.listen(port, () => {
     console.clear();
